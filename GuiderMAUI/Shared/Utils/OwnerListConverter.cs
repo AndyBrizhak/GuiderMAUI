a03b78dd247bb1efaf11f43e9c09d4bdeb397f89
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-12-20 09:45:05 +0800

    transfer  utilities for convertion

---
+++ GuiderMAUI/Shared/Utils/OwnerListConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using GuiderMAUI.Shared.Models; 
+
+namespace GuiderMAUI.Shared.Utils
+{
+    public class OwnerListConverter : JsonConverter<List<Owner>?>
+    {
+        public override List<Owner>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.Null: return null;
+                case JsonTokenType.StartObject:
+                    var single = JsonSerializer.Deserialize<Owner>(ref reader, options);
+                    return single != null ? new List<Owner> { single } : null;
+                case JsonTokenType.StartArray:
+                    return JsonSerializer.Deserialize<List<Owner>>(ref reader, options);
+                default:
+                    reader.Skip();
+                    return null;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, List<Owner>? value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value, options);
+        }
+    }
+}
